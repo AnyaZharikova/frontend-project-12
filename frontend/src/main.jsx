eaa64837507a653a21b9e72ce2f837179124ea9c
Anya Zharikova <kannie327@gmail.com>
2025-05-19 22:45:13 +0200

    change main.jsx for project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/* eslint-disable functional/no-expression-statement */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './Components/App.jsx';
+import store from './slices/index.js';
+
+const mountNode = document.getElementById('root');
+const root = ReactDOM.createRoot(mountNode);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
