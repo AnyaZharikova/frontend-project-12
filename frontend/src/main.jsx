e359d6e0a0070c8c24614d36592ba5d08bd1599a
Anya Zharikova <kannie327@gmail.com>
2025-05-21 21:00:40 +0200

    add token saving

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable functional/no-conditional-statement */
 /* eslint-disable functional/no-expression-statement */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,6 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './Components/App.jsx';
 import store from './slices/index.js';
+import { setCredentials } from './slices/authSlice.js';
+
+const tokenFromStorage = JSON.parse(localStorage.getItem('token'));
+const usernameFromStorage = JSON.parse(localStorage.getItem('username'));
+
+if (tokenFromStorage) {
+  store.dispatch(setCredentials({ username: usernameFromStorage, token: tokenFromStorage }));
+}
 
 const mountNode = document.getElementById('root');
 const root = ReactDOM.createRoot(mountNode);
