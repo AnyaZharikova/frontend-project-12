68409e6900f14486c37d66d33db00456b6a3703d
Anya Zharikova <kannie327@gmail.com>
2025-05-23 14:21:10 +0200

    fix error - merge storages

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/authSlice.js';
 import { chatApi } from './chatApi';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(chatApi.middleware),
