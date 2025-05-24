62f6e31f834fa9279b85c6f4535500ec1a44f54e
Anya Zharikova <kannie327@gmail.com>
2025-05-24 22:57:35 +0200

    add messageReducer

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice.js';
+import messageReducer from '../slices/messagesSlice.js';
 import { chatApi } from './chatApi';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    messages: messageReducer,
     [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(chatApi.middleware),
