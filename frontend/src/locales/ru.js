6a193ef7970833adb313a84b78dc2f15da3a5659
Anya Zharikova <kannie327@gmail.com>
2025-05-24 23:12:21 +0200

    add locales

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,38 @@
+export default {
+  translation: {
+    placeholders: {
+      login: 'Логин',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      passwordConfirmation: 'Подтвердите пароль',
+      newMessage: 'Введите новое сообщение...',
+    },
+    registrarion: 'Регистрация',
+    makeRegistration: 'Зарегистрироваться',
+    entry: 'Войти',
+    exit: 'Выйти',
+    noAccount: 'Нет аккаунта?',
+    notFoundPage: 'Страница не найдена',
+    channels: 'Каналы',
+    messagesCount: {
+      message_zero: 'Нет сообщений',
+      message_one: '{{count}} сообщение',
+      message_few: '{{count}} сообщения',
+      message_many: '{{count}} сообщений',
+    },
+    messageBody: 'Сообщение не может быть пустым',
+    send: '',
+    registrationRules: {
+      name: 'От 3 до 20 символов',
+      password: 'Не менее 6 символов',
+      passwordConfirmation: 'Пароли должны совпадать',
+    },
+    errors: {
+      invalidFeedback: 'Неверные имя пользователя или пароль',
+      userExist: 'Такой пользователь уже существует',
+      required: 'Обязательное поле',
+      network: 'Ошибка соединения',
+      message: 'Ошибка добавления сообщения',
+    },
+  },
+};
