f33623a5ca822d38702a95ae1a5e85b607f37eb6
Anya Zharikova <kannie327@gmail.com>
2025-05-28 14:54:03 +0200

    add socket event handlers for chat updates

--- frontend/src/services/socketHandlers.js
+++ frontend/src/services/socketHandlers.js
@@ -1,9 +1,40 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable functional/no-conditional-statement */
 /* eslint-disable functional/no-expression-statement */
-import { addMessage } from '../slices/messagesSlice.js';
+import { chatApi } from './chatApi.js';
 
 const setupSocketHandlers = (socket, store) => {
   socket.on('newMessage', (message) => {
-    store.dispatch(addMessage(message));
+    store.dispatch(
+      chatApi.util.updateQueryData('getMessages', undefined, (draft) => {
+        draft.push(message);
+      }),
+    );
+  });
+
+  socket.on('newChannel', (channel) => {
+    store.dispatch(
+      chatApi.util.updateQueryData('getChannels', undefined, (draft) => {
+        draft.push(channel);
+      }),
+    );
+  });
+
+  socket.on('renameChannel', (updatedChannel) => {
+    store.dispatch(
+      chatApi.util.updateQueryData('editChannel', undefined, (draft) => {
+        const index = draft.findIndex((ch) => ch.id === updatedChannel.id);
+        if (index !== -1) {
+          draft[index].name = updatedChannel.name;
+        }
+      }),
+    );
+  });
+
+  socket.on('removeChannel', (channelId) => {
+    store.dispatch(
+      chatApi.util.updateQueryData('getChannels', undefined, (draft) => draft.filter((ch) => ch.id !== channelId)),
+    );
   });
 };
 
