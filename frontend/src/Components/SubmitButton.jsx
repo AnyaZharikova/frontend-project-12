fa5ad71525ef529d419e273f61716c1ba834e208
Anya Zharikova <kannie327@gmail.com>
2025-05-28 17:17:09 +0200

    add reusable InputField and SubmitButton Components

---
+++ frontend/src/Components/SubmitButton.jsx
@@ -0,0 +1,20 @@
+import { Button } from 'react-bootstrap';
+
+const SubmitButton = (props) => {
+  const { values } = props;
+  const { formik, title } = values;
+
+  return (
+    <div className="d-grid">
+      <Button
+        variant="primary"
+        type="submit"
+        disabled={formik.isSubmitting}
+      >
+        {title}
+      </Button>
+    </div>
+  );
+};
+
+export default SubmitButton;
