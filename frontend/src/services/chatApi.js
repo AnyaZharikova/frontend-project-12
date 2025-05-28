9c6507fb38feeccb4e550398960a21c157e2862a
Anya Zharikova <kannie327@gmail.com>
2025-05-28 14:43:22 +0200

    add new endpoints

--- frontend/src/services/chatApi.js
+++ frontend/src/services/chatApi.js
@@ -4,12 +4,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { apiPath } from '../routes';
 
-export const chatApi = createApi({
+const chatApi = createApi({
   reducerPath: 'chatApi',
   baseQuery: fetchBaseQuery({
     baseUrl: apiPath,
     prepareHeaders: (headers, { getState }) => {
-      const { token } = getState().auth;
+      const { token } = getState().authReducer;
 
       if (token) {
         headers.set('Authorization', `Bearer ${token}`);
@@ -20,6 +20,20 @@ export const chatApi = createApi({
   }),
   tagTypes: ['Channels', 'Messages'],
   endpoints: (builder) => ({
+    signupUser: builder.mutation({
+      query: (userData) => ({
+        url: '/signup',
+        method: 'POST',
+        body: userData,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (userData) => ({
+        url: '/login',
+        method: 'POST',
+        body: userData,
+      }),
+    }),
     getChannels: builder.query({
       query: () => '/channels',
       providesTags: ['Channels'],
@@ -29,20 +43,58 @@ export const chatApi = createApi({
       providesTags: ['Messages'],
     }),
     addMessage: builder.mutation({
-      query: (message) => ({ url: '/messages', method: 'POST', body: message }),
+      query: (message) => ({
+        url: '/messages',
+        method: 'POST',
+        body: message,
+      }),
       invalidatesTags: ['Messages'],
+    }),
+    addChannel: builder.mutation({
+      query: (channel) => ({
+        url: '/channels',
+        method: 'POST',
+        body: channel,
+      }),
+      invalidatesTags: ['Channels'],
+    }),
+    editChannel: builder.mutation({
+      query: (channel) => ({
+        url: `channels/${channel.id}`,
+        method: 'PATCH',
+        body: { name: channel.name },
+      }),
+      invalidatesTags: ['Channels'],
+    }),
+    removeChannel: builder.mutation({
+      query: (channelId) => ({
+        url: `channels/${channelId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Channels', 'Messages'],
     }),
   }),
 });
 
 const {
+  useSignupUserMutation,
+  useLoginUserMutation,
   useGetChannelsQuery,
   useGetMessagesQuery,
   useAddMessageMutation,
+  useAddChannelMutation,
+  useEditChannelMutation,
+  useRemoveChannelMutation,
 } = chatApi;
 
 export {
+  chatApi,
+  useSignupUserMutation as signupUserMutation,
+  useLoginUserMutation as loginUserMutation,
   useGetChannelsQuery as getChannelsQuery,
   useGetMessagesQuery as getMessagesQuery,
   useAddMessageMutation as addMessageMutation,
+  useAddChannelMutation as addChannelMutation,
+  useEditChannelMutation as editChannelMutation,
+  useRemoveChannelMutation as removeChannelMutation,
 };
