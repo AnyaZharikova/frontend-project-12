4ebd27303338e1c9f3dae3d6685da788a8b58ac1
Anya Zharikova <kannie327@gmail.com>
2025-05-28 14:59:28 +0200

    integrate i18n and socket in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,28 @@
 /* eslint-disable functional/no-expression-statement */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import App from './Components/App.jsx';
+import setupSocketHandlers from './services/socketHandlers.js';
 import store from './services/index.js';
 import initSocket from './services/socket.js';
-import setupSocketHandlers from './services/socketHandlers.js';
+import resources from './locales/index.js';
 import { setCredentials } from './slices/authSlice.js';
 
 const initApp = () => {
+  const i18n = i18next.createInstance();
+  i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
+
   const tokenFromStorage = localStorage.getItem('token');
   const usernameFromStorage = localStorage.getItem('username');
   const parsedToken = tokenFromStorage ? JSON.parse(tokenFromStorage) : null;
