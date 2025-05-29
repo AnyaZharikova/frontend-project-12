94807252c5d42a66bb5bb40e38ec03f5519bc1bf
Anya Zharikova <kannie327@gmail.com>
2025-05-29 14:31:46 +0200

    integrate Rollbar

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -7,6 +7,8 @@ import {
   Navigate,
 } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
+import React from 'react';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
@@ -16,6 +18,11 @@ import LoginPage from './LoginPage.jsx';
 import RegisterPage from './RegisterPage.jsx';
 import NotFound from './NotFound.jsx';
 import routes from '../routes.js';
+
+const rollbarConfig = {
+  accessToken: '1c500ba8f73244d4b91cd9442f6a2266',
+  environment: 'testenv',
+};
 
 const PrivatRoute = ({ children }) => {
   const token = useSelector((state) => state.authReducer.token);
@@ -27,31 +34,35 @@ const App = () => {
   const { t } = useTranslation();
 
   return (
-    <BrowserRouter>
-      <div className="d-flex flex-column h-100">
-        <Navbar expand="lg" variant="light" bg="white" className="shadow-sm">
-          <Container>
-            <Navbar.Brand as={Link} to={routes.chatPage}>{t('name')}</Navbar.Brand>
-          </Container>
-        </Navbar>
-        <Routes>
-          <Route path="/" element={<Navigate to={routes.chatPage} replace />} />
-          <Route
-            path={routes.chatPage}
-            element={(
-              <PrivatRoute>
-                <ChatPage />
-              </PrivatRoute>
-            )}
-          />
-          <Route path={routes.loginPage} element={<LoginPage />} />
-          <Route path={routes.registerPage} element={<RegisterPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <div className="d-flex flex-column h-100">
+            <Navbar expand="lg" variant="light" bg="white" className="shadow-sm">
+              <Container>
+                <Navbar.Brand as={Link} to={routes.chatPage}>{t('name')}</Navbar.Brand>
+              </Container>
+            </Navbar>
+            <Routes>
+              <Route path="/" element={<Navigate to={routes.chatPage} replace />} />
+              <Route
+                path={routes.chatPage}
+                element={(
+                  <PrivatRoute>
+                    <ChatPage />
+                  </PrivatRoute>
+                )}
+              />
+              <Route path={routes.loginPage} element={<LoginPage />} />
+              <Route path={routes.registerPage} element={<RegisterPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
 
-        <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
-      </div>
-    </BrowserRouter>
+            <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+          </div>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
