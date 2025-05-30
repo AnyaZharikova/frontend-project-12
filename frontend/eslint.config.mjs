548cf29a6ab4f14fbe606ff3eea6be3e8d1e63d8
Anya Zharikova <kannie327@gmail.com>
2025-05-30 15:10:48 +0200

    set new linter settings

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,53 @@
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    ignores: ['dist/**'], // исключаем dist
+  },
+  // Подключаем стиль
+  stylistic.configs.recommended,
+
+  // Конфиг для JS и JSX
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      js,
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+])
